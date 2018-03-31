3e3f24ae067ffd2fec1daaefa38628ea2bd9282a
mmorawa <32575904+mmorawa@users.noreply.github.com>
2018-03-31 17:30:15 +0200

    First button

--- Password_Manager/Form1.cs
+++ Password_Manager/Form1.cs
@@ -16,5 +16,10 @@ namespace Password_Manager
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Start.");
+        }
     }
 }
